d4a913908ba338b0d35bc294f93cda517651ac1d
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2018-09-25 17:24:03 -0500

    added delete post to reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from '../actions';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 
 export default function(state = {}, action) {
 	switch (action.type) {
@@ -11,6 +11,8 @@ export default function(state = {}, action) {
 		case FETCH_POSTS:
 			//takes an array and returns an object with keys specified by the string arg
 			return _.mapKeys(action.payload.data, 'id');
+		case DELETE_POST:
+			return _.omit(state, action.payload)
 		default:
 			return state;
 	}
